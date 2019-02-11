e3645de105a0245a92283b34d0dddf4343a84f43
shadowthebat <10837037+shadowthebat@users.noreply.github.com>
2019-02-11 12:22:25 -0500

    weather class with display method

--- feelslike.py
+++ feelslike.py
@@ -1,8 +1,62 @@
 import math
 import json
+import os
+import sys
+
+class Weather:
+    def __init__(self, name, country, lat, lon, temp, weather, feels, weather_d,
+                 label, deg, wspeed, humidity, vis, pressure, sunrise, sunset, time, date):
+        self.name = name
+        self.country = country
+        self.lat = lat
+        self.lon = lon
+        self.temp = temp
+        self.weather = weather
+        self.feels = feels
+        self.weather_d = weather_d
+        self.label = label
+        self.deg = deg
+        self.wspeed = wspeed
+        self.humidity = humidity
+        self.vis = vis
+        self.pressure = pressure
+        self.sunrise = sunrise
+        self.sunset = sunset
+        self.time = time
+        self.date = date
+
+    def display_weather(self):
+        '''
+            prints redable weather info
+        '''
+        os.system('clear')
+        print(f'{self.name}, {self.country}\n{self.lat}, {self.lon}')
+        print(f'Temperature:    {self.temp} C°')
+        print(f'Conditions :    {self.weather}')
+        print('-------------------------')
+        print()
+        print(f'Feels Like :    {self.feels} C°')
+        print(f'Description:    {self.weather_d}')
+        print('-------------------------')
+        print()
+        print(f'Wind       :    {self.label}, {self.deg}°')
+        print(f'W. Speed   :    {self.wspeed} km/h')
+        print(f'Humidity   :    {self.humidity} %')
+        print(f'Visibility :    {self.vis} km')
+        print(f'Pressure   :    {self.pressure} kPa')
+        print(f'Sunrise    :    {self.sunrise}')
+        print(f'Sunset     :    {self.sunset}')
+        print('-------------------------')
+        print()
+        print(f'Last Update:    {self.time}')
+        print(f'                {self.date}')
+        print()
+
 
 def t_format(x):
-    # adds 0 infront min or hour given is single digit
+    '''
+        adds 0 infront min or hour given is single digit
+    '''
     if x < 10:
         time = f'0{x}'
         return time
@@ -10,20 +64,26 @@ def t_format(x):
         return x
 
 def intervols():
-    # used to determin direction labels
+    '''
+        used to determin direction labels
+    '''
     x = 0
     while x < 360:
         print(x)
         x += 22.5
 
 def converter(x):
-    # converts from meter/sec to km/hour
+    '''
+        converts from meter/sec to km/hour
+    '''
     x = (x*60)*60
     x = x/1000
     return round(x)
 
 def feels(wind,temp, humidity):
-    # Calculations from official canada website
+    '''
+        Calculates feels like conditions windchill/humidex
+    '''
     if wind >= 6 and temp > -50 and temp <= 5:
         chill=(13.12+0.6215*temp-11.37*(wind**0.16)+0.3965*temp*(wind**0.16))
         return round(chill)
@@ -37,7 +97,9 @@ def feels(wind,temp, humidity):
         return humidex
 
 def findid(x, id_dic):
-    # Finds city id by name
+    '''
+        Offers a choice between similar named cities
+    '''
     with open('cityid.json','r') as f:
         cities = f.read()
     cities = json.loads(cities)
@@ -52,19 +114,67 @@ def findid(x, id_dic):
             print()
             id_dic[str(choose_count)] = i['id']
             choose_count += 1
+    if choose_count == 1:
+        print(f'Sorry, no results found for {x}')
+        return sys.exit()
+    else:
+        choice = input('Enter choice: ')
+        return choice
+
+def wind_label(wdeg):
+    """
+        compares degree of wind to determin appropriate label
+        """
+    if wdeg >= 338 or wdeg < 23:
+        return 'N'
+    elif wdeg in range(23, 68):
+        return 'NE'
+    elif wdeg in range(68, 113):
+        return 'E'
+    elif wdeg in range(113, 158):
+        return 'SE'
+    elif wdeg in range(158, 203):
+        return 'S'
+    elif wdeg in range(203, 248):
+        return 'SW'
+    elif wdeg in range(248, 293):
+        return 'W'
+    elif wdeg in range(293, 338):
+        return 'NW'
 
 
+def create_url(y,key):
+    '''
+        Creates appropriate url based on sys.argv(y)
+    '''
+    if len(y) > 1:
+        if len(y) > 2:
+            if y[1] == 'find':
+                id_dic = {}
+                if len(y) > 3:
+                    x = y[2:]
+                    x = ' '.join(x)
+                    x = x.upper()
+                    choice = findid(x, id_dic)
+                    id = id_dic[choice]
+                    return f'http://api.openweathermap.org/data/2.5/weather?id={id}&appid={key}&units=metric'
+        
+                else:
+                    x = y[2].upper()
+                    choice = findid(x, id_dic)
+                    id = id_dic[choice]
+                    return f'http://api.openweathermap.org/data/2.5/weather?id={id}&appid={key}&units=metric'
+            
+            else:
+                city = y[1:]
+                city = '%20'.join(city)
+                return f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=metric'
 
-
-
-
-
-
-
-
-
-
-
+        else:
+            city = y[1]
+            return f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=metric'
+    else:
+        return f'http://api.openweathermap.org/data/2.5/weather?q=montreal,ca&appid={key}&units=metric'
 
 
 
--- weather.py
+++ weather.py
@@ -2,42 +2,11 @@ import requests
 import json
 from key import key
 from datetime import datetime
-import os
 import sys
 from feelslike import *
 
-if len(sys.argv) > 1:
-    if len(sys.argv) > 2:
-        if sys.argv[1] == 'find':
-            id_dic = {}
-            if len(sys.argv) > 3:
-                x = sys.argv[2:]
-                x = ' '.join(x)
-                x = x.upper()
-                findid(x, id_dic)
-                choice = input('Enter choice: ')
-                id = id_dic[choice]
-                apiurl = f'http://api.openweathermap.org/data/2.5/weather?id={id}&appid={key}&units=metric'
-                source = requests.get(apiurl) # response RAW
-                data = source.json() # response --> Python Dictionary
-        
-            else:
-                x = sys.argv[2].upper()
-                findid(x, id_dic)
-                choice = input('Enter choice: ')
-                id = id_dic[choice]
-                apiurl = f'http://api.openweathermap.org/data/2.5/weather?id={id}&appid={key}&units=metric'
-        
-        else:
-            city = sys.argv[1:]
-            city = '%20'.join(city)
-            apiurl = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=metric'
-    
-    else:
-        city = sys.argv[1]
-        apiurl = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=metric'
-else:
-    apiurl = f'http://api.openweathermap.org/data/2.5/weather?q=montreal,ca&appid={key}&units=metric'
+
+apiurl = create_url(sys.argv,key)
 source = requests.get(apiurl) # response RAW
 data = source.json() # response --> Python Dictionary
 
@@ -65,57 +34,25 @@ riseh = sunrise.hour
 risem = sunrise.minute
 riseh = t_format(riseh)
 risem = t_format(risem)
+sunrise = f'{riseh}:{risem}'
 sunset = data['sys']['sunset']
 sunset = datetime.fromtimestamp(sunset)
 seth = sunset.hour
 setm = sunset.minute
 seth = t_format(seth)
 setm = t_format(setm)
+sunset = f'{seth}:{setm}'
 
 # wind
 wspeed = data['wind']['speed']
 wspeed = converter(wspeed) # from meter/sec to km/h
 feels = feels(wspeed, temp, humidity)
-wdeg = data['wind']['deg']
-deg = wdeg
-# compares degree of wind to determin appropriate label
-if wdeg >= 338 or wdeg < 23:
-    d = 'N'
-elif wdeg in range(23, 68):
-    d = 'NE'
-elif wdeg in range(68, 113):
-    d = 'E'
-elif wdeg in range(113, 158):
-    d = 'SE'
-elif wdeg in range(158, 203):
-    d = 'S'
-elif wdeg in range(203, 248):
-    d = 'SW'
-elif wdeg in range(248, 293):
-    d = 'W'
-elif wdeg in range(293, 338):
-    d = 'NW'
-wdeg = d
-os.system('clear')
-# prints redable weather info
-print(f'{name}, {country}\n{lat}, {lon}')
-print(f'Temperature:    {temp} C°')
-print(f'Conditions :    {weather}')
-print('-------------------------')
-print()
-print(f'Feels Like :    {feels} C°')
-print(f'Description:    {weather_d}')
-print('-------------------------')
-print()
-print(f'Wind       :    {d}, {deg}°')
-print(f'W. Speed   :    {wspeed} km/h')
-print(f'Humidity   :    {humidity} %')
-print(f'Visibility :    {vis} km')
-print(f'Pressure   :    {pressure} kPa')
-print(f'Sunrise    :    {riseh}:{risem}')
-print(f'Sunset     :    {seth}:{setm}')
-print('-------------------------')
-print()
-print(f'Last Update:    {time}')
-print(f'                {date}')
-print()
+deg = data['wind']['deg']
+label = wind_label(deg)
+
+# current weather class
+current = Weather(name, country, lat, lon, temp, weather, feels, weather_d,
+                  label, deg, wspeed, humidity, vis, pressure, sunrise, sunset, time, date)
+
+# display current weather
+current.display_weather()
